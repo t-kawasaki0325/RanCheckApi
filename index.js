4ca8158d09b4da0317ab127e98bf9c8a09120695
Takahiro Kawasaki <takahiro.kawasaki0325@gmail.com>
2020-12-05 11:37:32 +0900

    feat: instanceの起動・停止が行える

---
+++ index.js
@@ -0,0 +1,75 @@
+const AWS = require('aws-sdk');
+const INSTANCE_ID = ['i-052d1395c28876647'];
+AWS.config.region = 'ap-northeast-1';
+
+const startInstance = async (ec2, params) => {
+  await ec2
+    .startInstances(params, (err) => {
+      if (err) {
+        throw {
+          code: 500,
+          stack: err.stack,
+          message: 'インスタンス起動に失敗しました',
+        };
+      }
+    })
+    .promise();
+  const { Reservations } = await new Promise((resolve) =>
+    ec2.waitFor('instanceRunning', params, (err, data) => {
+      if (err) {
+        throw {
+          code: 500,
+          stack: err.stack,
+          message: 'インスタンス起動待機に失敗しました',
+        };
+      } else {
+        resolve(data);
+      }
+    })
+  );
+  await ec2
+    .waitFor('instanceStatusOk', params, (err) => {
+      if (err) {
+        throw {
+          code: 500,
+          stack: err.stack,
+          message: 'ステータスok待機に失敗しました',
+        };
+      }
+    })
+    .promise();
+
+  return Reservations.map((reservation) =>
+    reservation.Instances.map((instance) => instance.PublicIpAddress).shift()
+  );
+};
+
+const stopInstance = (ec2, params) => {
+  ec2.stopInstances(params, (err) => {
+    if (err) {
+      throw {
+        code: 500,
+        stack: err.stack,
+        message: 'インスタンス停止に失敗しました',
+      };
+    }
+  });
+};
+
+const main = async () => {
+  const ec2 = new AWS.EC2();
+  const params = {
+    InstanceIds: INSTANCE_ID,
+  };
+
+  // インスタンスの開始
+  const ipAddresses = await startInstance(ec2, params);
+
+  // ここに処理を書く
+  console.log(ipAddresses);
+
+  // インスタンスの停止
+  stopInstance(ec2, params);
+};
+
+main();
